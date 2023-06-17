b15f8d15d20d308bed73687731a599fcb3bf1d10
jazepsR <jazepsrutkis93@gmail.com>
2023-06-17 10:03:05 +0300

    *Changed range

--- Assets/Scripts/ObjectManager.cs
+++ Assets/Scripts/ObjectManager.cs
@@ -78,7 +78,7 @@ public class ObjectManager : MonoBehaviour
         var distance =location.DistanceTo(new Coordinates(obj.LocationOptions.GetLocation().Latitude,
             obj.LocationOptions.GetLocation().Longitude), UnitOfLength.Kilometers )*1000f;
 
-        poorConnectionIndicator.SetActive(currentAccuracy > accuracyTreshold || distance > 100);
+        poorConnectionIndicator.SetActive(currentAccuracy > accuracyTreshold || distance > 500);
         distanceBar.gameObject.SetActive(currentAccuracy <= accuracyTreshold && obj.PlacementOptions.showDistance <= distance && 100 > distance );
         clickOnGhostText.gameObject.SetActive(obj.PlacementOptions.showDistance > distance);
 
